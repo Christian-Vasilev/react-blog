a5c5f30d74bca56f0f61e6c87c321be8b20fb47c
Christian-Vasilev <KristianVasilevv@gmail.com>
2020-04-22 09:55:48 +0300

    Bypassing props with context for theme. Added styles for dark and light mode [WIP] Added Helmet to modify <head> and <body> tags

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './sass/app.scss'
-import { FaBars } from "react-icons/all";
+import { FaBars, FaMoon, FaSun } from "react-icons/all";
 import Homepage from "./components/Homepage";
+import { Helmet } from "react-helmet";
+import { ThemeProvider } from "./contexts/theme";
+import { ThemeConsumer } from "./contexts/theme";
 
 class Nav extends React.Component {
-    state = { expandMobile: false }
+    state = {
+        expandMobile: false,
+    }
 
     click = () => this.setState(({ expandMobile }) => {
         return { expandMobile: !expandMobile }
@@ -15,36 +20,61 @@ class Nav extends React.Component {
         const { expandMobile } = this.state;
 
         return (
-            <header>
-                <div className='logo'>Christian</div>
-                <nav>
-                    <ul className='navbar'>
-                        <li className={`item item-dark${ expandMobile ? ' show' : '' }`}>Home</li>
-                        <li className={`item item-dark${ expandMobile ? ' show' : '' }`}>Blog</li>
-                        <li className={`item item-dark${ expandMobile ? ' show' : '' }`}>Portfolio</li>
-                        <li className={`item item-dark${ expandMobile ? ' show' : '' }`}>Contact</li>
-                        <li className='toggle'>
-                            <button
-                                type='button'
-                                className='btn btn-dark'
-                                onClick={this.click}>
-                                <FaBars size={22}/>
-                            </button>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
+            <ThemeConsumer>
+                { ({ theme, toggleTheme }) => (
+                    <header>
+                        <div className='logo'>Christian</div>
+                        <nav>
+                            <ul className='navbar'>
+                                <li className={`item item-dark${ expandMobile ? ' show' : '' }`}>Home</li>
+                                <li className={`item item-dark${ expandMobile ? ' show' : '' }`}>Blog</li>
+                                <li className={`item item-dark${ expandMobile ? ' show' : '' }`}>Portfolio</li>
+                                <li className={`item item-dark${ expandMobile ? ' show' : '' }`}>Contact</li>
+                                <li className={`item ${ expandMobile ? ' show' : '' }`}>
+                                    <button className='btn-reset' onClick={toggleTheme}>
+                                        {theme === 'light' ? <FaMoon size={16} /> : <FaSun size={16} />}
+                                    </button>
+                                </li>
+                                <li className='toggle'>
+                                    <button
+                                        type='button'
+                                        className='btn btn-dark'
+                                        onClick={this.click}>
+                                        <FaBars size={22}/>
+                                    </button>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
+                )}
+            </ThemeConsumer>
         );
     }
 }
 
-function App() {
-    return (
-        <React.Fragment>
-            <Nav />
-            <Homepage />
-        </React.Fragment>
-    )
+class App extends React.Component {
+    state = {
+        theme: 'dark',
+        toggleTheme: () => {
+            this.setState(({ theme }) => ({
+                theme: theme === 'light' ? 'dark' : 'light'
+            }))
+        }
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Helmet>
+                    <body data-theme={this.state.theme} />
+                </Helmet>
+                <ThemeProvider value={this.state}>
+                    <Nav />
+                    <Homepage />
+                </ThemeProvider>
+            </React.Fragment>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
